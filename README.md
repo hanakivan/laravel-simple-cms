d769fa6b6e4e583538fa274df9a6177c036d5cbd
Ivan hanák <hanakivan@gmail.com>
2022-02-06 15:31:55 +0100

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+## Requires
+- Laravel >= 8.0
+- php >= 8.0
+
+Will probably work with older versions, but untested.
+
+This package has been made using the following tutorial: https://devdojo.com/devdojo/how-to-create-a-laravel-package
+
+## Licensing
+👉 [See license](LICENSE.md)
---
+++ src/LaravelSimpleCMSServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace hanakivan\LaravelSimpleCms;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelSimpleCMSServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
